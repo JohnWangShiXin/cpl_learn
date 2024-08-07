5fb402001833145566db08556108d40078392757
John Wang <johnwangxtu@gmail.com>
2024-08-07 16:18:41 +0800

    bool type

---
+++ linux-c/ch2/bool.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+int main()
+{
+    bool a = false;
+    bool b = true;
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+
+    exit(0);
+}
